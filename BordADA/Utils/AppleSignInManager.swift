4c64e773d9551dd88ed3102f12faae4157450ead
Andre <andre_fabriciow@hotmail.com>
2024-10-05 02:27:57 -0300

    Implement managing user and game CRUD

--- BordADA/Utils/AppleSignInManager.swift
+++ BordADA/Utils/AppleSignInManager.swift
@@ -9,116 +9,123 @@ import Foundation
 import FirebaseAuth
 import AuthenticationServices
 import CryptoKit
-import SwiftUI
 
-class AppleSignInManager: NSObject, ObservableObject, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+class AppleSignInManager: NSObject, ObservableObject {
     @Published var currentNonce: String?
-
-    // Função para iniciar o login com Apple com um completion handler
-    func startSignInWithAppleFlow(completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+    
+    // Função para iniciar o fluxo de login com Apple configurando o request
+    func startSignInWithAppleFlow(request: ASAuthorizationAppleIDRequest) {
         let nonce = randomNonceString()
         currentNonce = nonce
-        let request = createAppleIDRequest(with: nonce)
-        
-        let authController = ASAuthorizationController(authorizationRequests: [request])
-        authController.delegate = self
-        authController.presentationContextProvider = self
-        self.completionHandler = completion
-        authController.performRequests()
-    }
-
-    private var completionHandler: ((Result<AuthDataResult, Error>) -> Void)?
-
-    // Criação do pedido de login com Apple
-    private func createAppleIDRequest(with nonce: String) -> ASAuthorizationAppleIDRequest {
-        let request = ASAuthorizationAppleIDProvider().createRequest()
         request.requestedScopes = [.fullName, .email]
-        request.nonce = sha256(nonce)
-        return request
+        request.nonce = sha256(nonce) // Sempre usar o hash SHA256 do nonce
+    }
+    
+    func handle(authorization: ASAuthorization, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Falha de validação do nonce. Uma nova autenticação foi solicitada sem um nonce.")
+            }
+            
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Erro ao obter o token de identidade do Apple ID.")
+                completion(.failure(NSError(domain: "AppleSignIn", code: -1, userInfo: [NSLocalizedDescriptionKey: "Erro ao obter token de identidade"])))
+                return
+            }
+            
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Não foi possível converter o token de identidade em uma string.")
+                completion(.failure(NSError(domain: "AppleSignIn", code: -1, userInfo: [NSLocalizedDescriptionKey: "Erro ao converter token de identidade"])))
+                return
+            }
+            
+            // Configurar credenciais Firebase com o token de identidade
+            let credential = OAuthProvider.credential(
+                providerID: AuthProviderID.apple,
+                idToken: idTokenString,
+                rawNonce: nonce
+            )
+            
+            // Autenticar no Firebase
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let error = error {
+                    print("Erro ao autenticar com Firebase: \(error.localizedDescription)")
+                    completion(.failure(error))
+                    return
+                }
+                
+                if let authResult = authResult {
+                    
+                
+                    // Capturar o nome completo do usuário
+                    if let fullName = appleIDCredential.fullName {
+                        
+                        Task {
+                            do {
+                                if await !UserManager().checkIfPlayerExists() {
+                                    let displayName = [fullName.givenName, fullName.familyName]
+                                        .compactMap { $0 }
+                                        .joined(separator: " ")
+                                    
+                                    let changeRequest = authResult.user.createProfileChangeRequest()
+                                    changeRequest.displayName = displayName
+                                    changeRequest.commitChanges { error in
+                                        if let error = error {
+                                            print("Erro ao atualizar o nome do usuário: \(error.localizedDescription)")
+                                        } else {
+                                            print("Nome do usuário atualizado para: \(displayName)")
+                                        }
+                                    }
+                                }
+                                
+                            }
+                           
+                        }
+
+                    }
+                    
+                    // Autenticação bem-sucedida
+                    print("Usuário autenticado com sucesso: \(authResult.user.uid)")
+                    completion(.success(authResult))
+                }
+            }
+        }
     }
 
-    // Função auxiliar para gerar um nonce aleatório (necessário para segurança)
     private func randomNonceString(length: Int = 32) -> String {
-        let charset: Array<Character> = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        precondition(length > 0)
+        let charset: [Character] =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
         var result = ""
         var remainingLength = length
 
         while remainingLength > 0 {
-            let randoms: [UInt8] = (0..<16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError("Não foi possível gerar nonce aleatório")
-                }
-                return random
+            var randomBytes = [UInt8](repeating: 0, count: 16)
+            let status = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+            if status != errSecSuccess {
+                fatalError("Não foi possível gerar nonce aleatório. Código de erro: \(status)")
             }
 
-            randoms.forEach { random in
+            randomBytes.forEach { randomByte in
                 if remainingLength == 0 {
                     return
                 }
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
+
+                // Seleciona um caractere aleatório do charset
+                let index = Int(randomByte) % charset.count
+                result.append(charset[index])
+                remainingLength -= 1
             }
         }
 
         return result
     }
-
-    // Função auxiliar para realizar o hash SHA256 da nonce
+    
     private func sha256(_ input: String) -> String {
         let inputData = Data(input.utf8)
         let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap { String(format: "%02x", $0) }.joined()
+        // Converte os bytes hash em uma string hexadecimal
+        let hashString = hashedData.map { String(format: "%02x", $0) }.joined()
         return hashString
-    }
-
-    // Callback do ASAuthorizationController para autenticação bem-sucedida
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
-            print("Erro ao obter as credenciais do Apple ID")
-            self.completionHandler?(.failure(NSError(domain: "AppleSignIn", code: -1, userInfo: [NSLocalizedDescriptionKey: "Erro ao obter credenciais"])))
-            return
-        }
-
-        guard let nonce = currentNonce else {
-            fatalError("Falha de validação do nonce. Uma nova autenticação foi solicitada sem um nonce.")
-        }
-
-        guard let appleIDToken = appleIDCredential.identityToken else {
-            print("Erro ao obter o token de identidade do Apple ID.")
-            self.completionHandler?(.failure(NSError(domain: "AppleSignIn", code: -1, userInfo: [NSLocalizedDescriptionKey: "Erro ao obter token de identidade"])))
-            return
-        }
-
-        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-            print("Não foi possível converter o token de identidade em uma string.")
-            self.completionHandler?(.failure(NSError(domain: "AppleSignIn", code: -1, userInfo: [NSLocalizedDescriptionKey: "Erro ao converter token de identidade"])))
-            return
-        }
-
-        // Configurar credenciais Firebase com o token de identidade
-        let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
-
-        // Autenticar no Firebase
-        Auth.auth().signIn(with: credential) { (authResult, error) in
-            if let error = error {
-                print("Erro ao autenticar com Firebase: \(error.localizedDescription)")
-                self.completionHandler?(.failure(error))
-                return
-            }
-
-            if let authResult = authResult {
-                // Autenticação bem-sucedida
-                print("Usuário autenticado com sucesso: \(authResult.user.uid)")
-                self.completionHandler?(.success(authResult))
-            }
-        }
-    }
-
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return UIApplication.shared.windows.first { $0.isKeyWindow }!
     }
 }
